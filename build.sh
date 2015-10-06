39c22ea71a1fdd2d6877d3ed954c504af829771b
Łukasz Jan Niemier <lukasz@niemier.pl>
2015-10-06 17:45:05 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 hugo && echo || exit 1
 
-export GIT_WORK_TREE="$(pwd)/public"
-export GIT_DIR="$GIT_WORK_TREE/.git"
+pushd ./public
 
 URL="$(git config remote.origin.url)"
 
@@ -17,3 +16,5 @@ fi
 git add -A
 git commit -m "${1:-"Update $(env LANG=en_US date)"}"
 git push -qf "$URL" master
+
+popd
